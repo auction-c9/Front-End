be99cdafd14d121e46c6974878a7c81b39f9076d
phuong6898 <phuong060898@gmail.com>
2025-03-18 13:01:40 +0700

    profile

--- auction-front-end/src/App.js
+++ auction-front-end/src/App.js
@@ -15,6 +15,7 @@ import Register from "./pages/login/Register";
 import ForgotPasswordStep3 from "./pages/login/ForgotPasswordStep3";
 import ForgotPasswordStep2 from "./pages/login/ForgotPasswordStep2";
 import ForgotPasswordStep1 from "./pages/login/ForgotPasswordStep1";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 const App = () => {
     useEffect(() => {
@@ -32,10 +33,10 @@ const App = () => {
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/logout" element={<Logout />} />
-                    <Route path="/profile" element={<Profile />} />
                     <Route path="/forgot-password" element={<ForgotPasswordStep1 />} />
                     <Route path="/forgot-password/step2" element={<ForgotPasswordStep2 />} />
                     <Route path="/forgot-password/step3" element={<ForgotPasswordStep3 />} />
+                    <Route path="/profile" element={<ProfilePage />} />
                 </Routes>
             </AuthProvider>
         </Router>
