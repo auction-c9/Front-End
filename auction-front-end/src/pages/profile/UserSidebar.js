037e4e1a0b66855ddd46819cba556be310b84c6c
phuong6898 <phuong060898@gmail.com>
2025-03-19 22:32:48 +0700

    auction_history
    # Conflicts:
    #	auction-front-end/src/config/apiConfig.js

---
+++ auction-front-end/src/pages/profile/UserSidebar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaUser, FaHistory, FaGavel } from "react-icons/fa";
+
+const UserSidebar = () => {
+    return (
+        <div className="user-sidebar">
+            <nav>
+                <Link to="/profile">
+                    <FaUser /> Quản lý trang cá nhân
+                </Link>
+                <Link to="/auction-register">
+                    <FaHistory /> Lịch sử đăng ký đấu giá
+                </Link>
+                <Link to="/profile/auction-history">
+                    <FaGavel /> Lịch sử đấu giá mặt hàng
+                </Link>
+            </nav>
+        </div>
+    );
+};
+
+export default UserSidebar;
