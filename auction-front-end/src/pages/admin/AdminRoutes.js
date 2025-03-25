4e74dc4fe0d7beee5c9f9c2722f6686465440911
phuongthao0612 <phuong6thao@gmail.com>
2025-03-25 13:14:37 +0700

    update

--- auction-front-end/src/pages/admin/AdminRoutes.js
+++ auction-front-end/src/pages/admin/AdminRoutes.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import AdminDashboard from './AdminDashboard';
 import AdminCustomerList from './AdminCustomerList';
@@ -7,32 +7,25 @@ import AdminProductList from "./AdminProductList";
 import AdminTransactionList from "./AdminTransactionList";
 
 const AdminRoutes = () => {
-    const { user } = useAuth();
-    const [isAuthChecked, setIsAuthChecked] = useState(false);
+    const { user, loading } = useAuth();
 
-    useEffect(() => {
-        if (user) {
-            setIsAuthChecked(true);
-        }
-    }, [user]);
-
-    if (!isAuthChecked) {
-        return null;
+    if (loading) {
+        return <div>Loading...</div>;
     }
 
     if (!user) {
-        return <Navigate to="/login" />;
+        return <Navigate to="/login" replace />;
     }
 
     if (user.role !== "ROLE_ADMIN") {
-        return <Navigate to="/" />;
+        return <Navigate to="/" replace />;
     }
 
     return (
         <Routes>
             <Route path="/" element={<AdminDashboard />} />
             <Route path="/customers" element={<AdminCustomerList />} />
-            <Route path="/products" element={<AdminProductList/>} />
+            <Route path="/products" element={<AdminProductList />} />
             <Route path="/transactions" element={<AdminTransactionList />} />
         </Routes>
     );
